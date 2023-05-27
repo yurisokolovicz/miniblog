0ff3c335c16d7e83b0bee35d1c676b7ecc10963d
yurisokolovicz <yurisokolovicz@gmail.com>
2023-05-26 22:03:02 -0300

    Configuring React Router

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import "./App.css";
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
+
+import Home from './pages/Home/Home';
+import About from './pages/About/About';
+import './App.css';
 
 const App = () => {
-  return (
-    <div className="App">
-      <h1>MiniBlog</h1>
-    </div>
-  );
+    return (
+        <div className="App">
+            <BrowserRouter>
+                <div className="container">
+                    <Routes>
+                        <Route path="/" element={<Home />} />
+                        <Route path="/about" element={<About />} />
+                    </Routes>
+                </div>
+            </BrowserRouter>
+        </div>
+    );
 };
 
 export default App;
