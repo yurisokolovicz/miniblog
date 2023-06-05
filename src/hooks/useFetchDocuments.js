c87919c368cbcb34b888bfc1ffa90942b022bdc6
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-05 15:12:19 -0300

    Custom Hook for fetch data from firestore.

---
+++ src/hooks/useFetchDocuments.js
@@ -0,0 +1,61 @@
+import { useState, useEffect } from 'react';
+import { db } from '../firebase/config';
+import { collection, query, orderBy, onSnapshot, where, QuerySnapshot } from 'firebase/firestore';
+
+// We did not use useReducer here because we are not going to handle any reducer events (actions). We are just going to fetch data from the database.
+export const useFetchDocuments = (docCollection, search = null, uid = null) => {
+    const [documents, setDocuments] = useState(null);
+    const [error, setError] = useState(null);
+    const [loading, setLoading] = useState(false);
+
+    // Deal with memory leaks
+    const [cancelled, setCancelled] = useState(false);
+
+    useEffect(() => {
+        async function loadData() {
+            if (cancelled) return;
+
+            setLoading(true);
+
+            const collectionRef = await collection(db, docCollection);
+
+            try {
+                let q;
+
+                // busca
+
+                //dashboard
+
+                q = await query(collectionRef, orderBy('createdAt', 'desc'));
+
+                await onSnapshot(q, QuerySnapshot => {
+                    setDocuments(
+                        QuerySnapshot.docs.map(doc => ({
+                            id: doc.id,
+                            ...doc.data()
+                        }))
+                    );
+                });
+
+                setLoading(false);
+            } catch (error) {
+                console.log(error);
+                setError(error.message);
+
+                setLoading(false);
+            }
+        }
+
+        loadData(); // We need to call the function to execute it, it will only be executed if the dependencies change (doCollection, search, uid, cancelled).
+    }, [docCollection, search, uid, cancelled]);
+
+    // Deal with memory leaks
+    // The return function will be executed when the component is unmounted. It prevents the component from trying to set the state after it has been unmounted.
+    useEffect(() => {
+        return () => {
+            setCancelled(true);
+        };
+    }, []);
+
+    return documents, loading, error;
+};
