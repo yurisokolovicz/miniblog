7ee83f390e0bc8c11c09d58710427a0039a1ced3
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-05 17:31:05 -0300

    Displaying Search Results

--- src/pages/Search/Search.js
+++ src/pages/Search/Search.js
@@ -1,16 +1,32 @@
 import { useFetchDocuments } from '../../hooks/useFetchDocuments';
 import { useQuery } from '../../hooks/useQuery';
+import { Link } from 'react-router-dom';
 
+import PostDetail from '../../components/PostDetail';
 import styles from './Search.module.css';
 
 const Search = () => {
     const query = useQuery();
     const search = query.get('q');
 
+    // Load the docs according to the search term
+    const { documents: posts } = useFetchDocuments('posts', search);
+
+    // post is passed as props
     return (
-        <div>
+        <div className={styles.search_container}>
             <h2>Search</h2>
-            <p>{search}</p>
+            <div>
+                {posts && posts.length === 0 && (
+                    <div className={styles.noposts}>
+                        <p>No posts were found</p>
+                        <Link to="/" className="btn btn-dark btn-search">
+                            Return
+                        </Link>
+                    </div>
+                )}
+                {posts && posts.map(post => <PostDetail key={post.id} post={post} />)}
+            </div>
         </div>
     );
 };
