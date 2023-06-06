df1e5dc5629e56ec32f2a17d85f33744f20c60d2
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-06 11:18:24 -0300

    Styling (CSS module) the Post.js Component

--- src/pages/Post/Post.js
+++ src/pages/Post/Post.js
@@ -1,18 +1,29 @@
 import { useParams } from 'react-router-dom';
 import { useFetchDocument } from '../../hooks/useFetchDocument';
 
-// import styles from './Post.module.css';
+import styles from './Post.module.css';
 
 const Post = () => {
     const { id } = useParams();
     const { document: post, loading } = useFetchDocument('posts', id);
 
     return (
-        <div>
+        <div className={styles.post_container}>
             {loading && <p>Loading post...</p>}
             {post && (
                 <>
                     <h1>{post.title}</h1>
+                    <img src={post.image} alt={post.title} />
+                    <p>{post.body}</p>
+                    <h3>This post is about:</h3>
+                    <div className={styles.tags}>
+                        {post.tagsArray.map(tag => (
+                            <p key={tag}>
+                                <span>#</span>
+                                {tag}
+                            </p>
+                        ))}
+                    </div>
                 </>
             )}
         </div>
