5661435227b167bf50a902c2c62bbcd65b7771b2
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-06 11:34:55 -0300

    Structuring Dashboard

--- src/pages/Dashboard/Dashboard.js
+++ src/pages/Dashboard/Dashboard.js
@@ -1,9 +1,32 @@
+import { Link } from 'react-router-dom';
+import { useAuthValue } from '../../context/AuthContext';
+import { useFetchDocuments } from '../../hooks/useFetchDocuments';
+
 import styles from './Dashboard.module.css';
 
 const Dashboard = () => {
+    const { user } = useAuthValue();
+    const uid = user.uid;
+
+    // user post
+    const posts = [];
+
     return (
         <div>
-            <h1>Dashboard</h1>;
+            <h2>Dashboard</h2>
+            <p>Manage your posts</p>
+            {posts && posts.length === 0 ? (
+                <div className={styles.noposts}>
+                    <p>No posts found</p>
+                    <Link to="/posts/create" className="btn btn-search">
+                        create first post
+                    </Link>
+                </div>
+            ) : (
+                <div>
+                    <p>Has a post</p>
+                </div>
+            )}
         </div>
     );
 };
