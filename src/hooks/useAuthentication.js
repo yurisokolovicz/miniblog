f015707a4b1b30b00ca5ad9886021e7c581756c7
yurisokolovicz <yurisokolovicz@gmail.com>
2023-06-06 16:22:16 -0300

    Web Version Ready for Deploy.

--- src/hooks/useAuthentication.js
+++ src/hooks/useAuthentication.js
@@ -1,6 +1,4 @@
 import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword, updateProfile, signOut } from 'firebase/auth';
-
-import { db } from '../firebase/config';
 import { useState, useEffect } from 'react';
 
 export const useAuthentication = () => {
